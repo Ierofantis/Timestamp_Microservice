b88699e7b82d3a14bc8c491fd3af2b40a5c3a8a6
Ierofantis <evil69669@gmail.com>
2016-06-10 15:04:43 +0300

    add

---
+++ README.md
@@ -0,0 +1,12 @@
+Timestamp_Microservice
+
+Timestamp Microservice that I can pass a string as a parameter, and it will check to see whether that string contains either a unix
+timestamp or a natural language date (example: January 1, 2016).
+
+If it does, it returns both the Unix timestamp and the natural language form of that date.
+
+If it does not contain a date or Unix timestamp, it returns null for those properties.
+
+It is written on Express.Js for Node.js
+
+You can see this app on http://immense-beach-35342.herokuapp.com/
---
+++ time.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var moment = require('moment');
+var path = require('path');
+
+const PORT = process.env.PORT || 8081;
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', function(req, res, next) {
+  var fileName = path.join(__dirname, 'index.html');
+  res.sendFile(fileName, function(error) {
+    // pass on a error message if there was an error
+    if (error) { next('File was not found'); }
+  })
+});
+
+app.get("/:date", function(req, res) {
+  var date = moment(req.params.date,"MMMM D, YYYY");
+
+  // Not getting called cause date returns valid even with unix timestamps
+  if (!date.isValid()) {
+    date = moment.unix(req.params.date);
+  }
+
+  if (!date.isValid()) {
+    res.json({
+      'correct': null,
+      'unix': null
+    });
+  }
+
+  res.json({
+    'correct': date.format('MMMM DD, YYYY'),
+    'unix': date.format('X')
+  });
+});
+
+app.listen(PORT, function (error) {
+  console.log("Server is listening on port %s", PORT)
+});
